96a9e10f4cdf0e9df6e10f70af63f6967b78cab4
gabrieljr94 <gabriel.laweh@spotlight.com>
2021-02-18 15:17:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi my name is Eminem!'));
+app.get('/', (req, res) => res.send('Testing this AWS thing out!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
